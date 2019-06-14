a22ae8ccd674d094ef10d4af0f5f8ecd664dd259
Mateusz Lewartowski <lewar2013c@gmail.com>
2019-06-14 19:42:08 +0200

    Changed addToList and added bind function

--- script.js
+++ script.js
@@ -11,15 +11,6 @@ class Stopwatch extends React.Component {
         }
     }
 
-    pad0(value) {
-        let result = value.toString();
-
-        if (result.length < 2) {
-            result = `0${result}`;
-        }
-        return result;
-    }
-
     format() {
         return `${this.pad0(this.state.times.minutes)}:${this.pad0(this.state.times.seconds)}:${this.pad0(Math.floor(this.state.times.miliseconds))}`;
     }
@@ -47,6 +38,29 @@ class Stopwatch extends React.Component {
         })
     }
 
+    reset() {
+        this.setState({
+            times: {
+                minutes: 0,
+                seconds: 0,
+                miliseconds: 0
+            }
+        })
+    }
+
+    addToList() {
+        document.querySelector(".stopwatch").classList.add("this-score");
+        let element = document.createElement('li');
+        element.innerText = this.format(this.state.times);
+        document.querySelector('.results').appendChild(element);
+        setTimeout(() => document.querySelector(".stopwatch").classList.remove("this-score"), 500);
+    }
+
+    clearList() {
+        let scoresList = document.querySelector('.results');
+        scoresList.innerText = '';
+    }
+
     calculate(times) {
         let result = times;
         result.miliseconds += 1;
@@ -63,45 +77,29 @@ class Stopwatch extends React.Component {
         return result;
     }
 
-    reset() {
-        this.setState({
-            times: {
-                minutes: 0,
-                seconds: 0,
-                miliseconds: 0
-            }
-        })
-    }
+    pad0(value) {
+        let result = value.toString();
 
-    addToList() {
-        this.display.classList.add('this-score');
-        let element = document.createElement('li');
-        element.innerText = this.print();
-
-        document.querySelector('.results').appendChild(element);
-        setTimeout(() => this.display.classList.remove('this-score'), 500);
-    }
-
-    clearList() {
-        let scoresList = document.querySelector(".results");
-        while (scoresList.hasChildNodes()) {
-            scoresList.removeChild(scoresList.lastChild)
+        if (result.length < 2) {
+            result = `0${result}`;
         }
+        return result;
     }
 
     render() {
         return (
-            <nav className="main">
-                <button onClick={this.start.bind(this)} id="start">Start</button>
-                <button onClick={this.stop.bind(this)} id="stop">Stop</button>
-                <button onClick={this.reset.bind(this)} id="reset">Reset</button>
-                <button onClick={this.addToList.bind(this)} id="add-score">Add score</button>
-                <button onClick={this.clearList.bind(this)} id="clear-score">Clear scores</button>
-                <div className="stopwatch">{this.format(this.state.times)}</div>
-                <ul className="results"></ul>
-            </nav>
+            React.createElement("container", { className: "container" },
+                React.createElement("a", { onClick: this.start.bind(this), className: "start" }, "Start"),
+                React.createElement("a", { onClick: this.stop.bind(this), className: "stop" }, "Stop"),
+                React.createElement("a", { onClick: this.reset.bind(this), className: "reset" }, "Reset"),
+                React.createElement("a", { onClick: this.addToList.bind(this), className: "add-score" }, "Add score"),
+                React.createElement("a", { onClick: this.clearList.bind(this), className: "clear" }, "Clear scores"),
+                React.createElement("div", { className: "stopwatch" }, this.format(this.state.times)),
+                React.createElement("ul", { className: "results" })
+            )
         )
     }
 }
 
-ReactDOM.render(<Stopwatch />, document.getElementById("app"));
+const app = React.createElement(Stopwatch);
+ReactDOM.render(app, document.getElementById("app"));
